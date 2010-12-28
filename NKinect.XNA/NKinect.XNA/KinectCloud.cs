ef9c914e488870f5bd6b8c98339c34603eb4633c
SND\bscheiman_cp <SND\bscheiman_cp@527b9aa4-23f3-4c7d-acba-bb8337ca51bc>
2010-12-28 08:05:07 +0000

    Realtime (2D) cloud generation
    git-svn-id: https://nkinect.svn.codeplex.com/svn@3970 527b9aa4-23f3-4c7d-acba-bb8337ca51bc

--- NKinect.XNA/NKinect.XNA/KinectCloud.cs
+++ NKinect.XNA/NKinect.XNA/KinectCloud.cs
@@ -1,49 +1,42 @@
 #region
-using System.Windows.Forms;
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using ButtonState = Microsoft.Xna.Framework.Input.ButtonState;
-using Keys = Microsoft.Xna.Framework.Input.Keys;
 
 #endregion
 
 namespace NKinect.XNA {
     public class KinectCloud : Game {
-        protected Vector3 CameraPosition = new Vector3(0.0f, 50.0f, 30000.0f);
-        protected Vector3 Position;
-        protected float Zoom;
-        protected float AspectRatio { get; set; }
-        protected Model Cube { get; set; }
+        protected double[][] Depths { get; set; }
         protected GraphicsDeviceManager Graphics { get; set; }
         protected BaseKinect Kinect { get; set; }
         protected float ModelRotation { get; set; }
-        protected Vector3[] Positions { get; set; }
+        protected Texture2D RectTexture { get; set; }
         protected SpriteBatch SpriteBatch { get; set; }
-        
+
         public KinectCloud() {
             Graphics = new GraphicsDeviceManager(this) {
-                IsFullScreen = true,
-                PreferredBackBufferWidth = Screen.PrimaryScreen.Bounds.Width,
-                PreferredBackBufferHeight = Screen.PrimaryScreen.Bounds.Height
+                IsFullScreen = false,
+                PreferredBackBufferWidth = 640,
+                //Screen.PrimaryScreen.Bounds.Width,
+                PreferredBackBufferHeight = 480 //Screen.PrimaryScreen.Bounds.Height
             };
 
             Content.RootDirectory = "Content";
             Kinect = KinectFactory.GetKinect();
+
+            Kinect.DepthsCalculated += KinectDepthsCalculated;
+
+            Kinect.Start();
+        }
+
+        private void KinectDepthsCalculated(object sender, DepthEventArgs e) {
+            Depths = e.Depths;
         }
 
         protected override void Initialize() {
-            //Positions = new Vector3[1000];
-
-            //for (int i = 0; i < Positions.Length; i++) {
-            //    var p = Positions[i];
-
-            //    p.X = i;
-            //    p.Y = i;
-            //    p.Z = 0;
-            //}
-
-            Position = Vector3.Zero;
+            Window.Title = "NKinect + XNA Test";
 
             base.Initialize();
         }
@@ -51,8 +44,7 @@ namespace NKinect.XNA {
         protected override void LoadContent() {
             SpriteBatch = new SpriteBatch(GraphicsDevice);
 
-            AspectRatio = Graphics.GraphicsDevice.Viewport.AspectRatio;
-            Cube = Content.Load<Model>("cube");
+            RectTexture = Content.Load<Texture2D>("rect");
         }
 
         protected override void UnloadContent() {
@@ -63,42 +55,46 @@ namespace NKinect.XNA {
                 Keyboard.GetState(PlayerIndex.One).IsKeyDown(Keys.Escape))
                 Exit();
 
-            if (Keyboard.GetState(PlayerIndex.One).IsKeyDown(Keys.Right))
-                Position.X += 10;
-
-            if (Keyboard.GetState(PlayerIndex.One).IsKeyDown(Keys.Left))
-                Position.X -= 10;
-
-            if (Keyboard.GetState(PlayerIndex.One).IsKeyDown(Keys.Up))
-                Position.Y += 10;
-
-            if (Keyboard.GetState(PlayerIndex.One).IsKeyDown(Keys.Down))
-                Position.Y -= 10;
-
-            CameraPosition.Z = 30000.0f - (Mouse.GetState().ScrollWheelValue * 10);
+            ProcessKeyboard();
+            ProcessMouse();
 
             base.Update(gameTime);
         }
 
+        private void ProcessMouse() {
+        }
+
+        private void ProcessKeyboard() {
+        }
+
         protected override void Draw(GameTime gameTime) {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            var transforms = new Matrix[Cube.Bones.Count];
-            Cube.CopyAbsoluteBoneTransformsTo(transforms);
+            SpriteBatch.Begin();
 
-            //foreach (var p in Positions) {
-            foreach (var mesh in Cube.Meshes) {
-                foreach (BasicEffect effect in mesh.Effects) {
-                    effect.EnableDefaultLighting();
-                    effect.World = transforms[mesh.ParentBone.Index] * Matrix.CreateRotationY(ModelRotation) *
-                                   Matrix.CreateTranslation(Position);
-                    effect.View = Matrix.CreateLookAt(CameraPosition, Vector3.Zero, Vector3.Up);
-                    effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f), AspectRatio, 1.0f, 100000.0f);
+            const int minDistance = -10;
+            const double scaleFactor = 0.0021;
+
+            if (Depths != null) {
+                for (int j = 0; j < 480; j++) {
+                    for (int i = 0; i < 640; i++) {
+                        double z = Depths[i][j];
+
+                        if (z > 1000)
+                            continue;
+
+                        double y = (640 / 2 - j) * (z + minDistance) * scaleFactor;
+                        double x = (i - 480 / 2) * (z + minDistance) * scaleFactor;
+                        var newDepth = (float) (Math.Abs(z) / 1000);
+                        int color = (int) (newDepth * 1000) / 4;
+
+                        SpriteBatch.Draw(RectTexture, new Vector2((float) x + 240, (float) (320 - y)), null, new Color(color, color, color), 0, Vector2.Zero, Vector2.One,
+                                         SpriteEffects.None, newDepth);
+                    }
                 }
-
-                mesh.Draw();
             }
-            //}
+
+            SpriteBatch.End();
 
             base.Draw(gameTime);
         }
